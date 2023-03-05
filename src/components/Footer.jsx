a5a1ea76abb995a3e8f6f7b3ad44489959ea35a2
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-03-05 22:19:19 +0300

    footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Logo from '../images/Logo.JPG'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <img src={Logo} alt=''/>
+      <span>Made with ❤️ and <b>React.js</b></span>
+    </footer>
   )
 }
 
